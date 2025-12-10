f29a1df56807f38a826935ecb254a39b743687fb
SamGombs20 <gombajosh18@gmail.com>
2025-12-10 12:29:40 +0300

    Update:Project set up

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
 import { NextPage } from "next";
 import Link from "next/link";
-
+import style from './about.module.css';
 const About:NextPage=()=>{
     return (
-        <div>
+        <div className={style.about_container}>
             <h3>This is the styled about page</h3>
             <Link href="/">to Home</Link>
         </div>
