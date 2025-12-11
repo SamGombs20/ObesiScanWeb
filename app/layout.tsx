01df3cb8819e654058dcee240dcb5614bd9a1166
SamGombs20 <gombajosh18@gmail.com>
2025-12-11 13:02:12 +0300

    Update:Working on the how component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import Navbar from "./components/Navbar";
 import "./globals.css"
 
@@ -11,6 +11,10 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+export const poppins = Poppins({
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,24 @@ export default function Main (){
     alert("Button clicked")
   }
   return(
-    <div>
+    <div className={style.home_container}>
       <div className={style.intro_container}>
         <p className={style.intro_text}>Obesi<span>Scan</span> - start your <em><strong>journey</strong></em> to better <em><strong>health</strong></em> today</p>
         <div className={style.check_container}>
           <p className={style.check_title}>Quickly check your obesity level</p>
           <p>We collect anonymous data of your personal, physical, diet and lifestyle information.</p>
           <button onClick={onButtonClick} className={style.check_btn}>Check now!</button>
+        </div>
+      </div>
+      <div className={style.how_container}>
+        <div className={style.container}>
+          <p className={style.title_text}>How it works</p>
+          <p>Our model predicts a person's likelihood of being  overweight or obese based on various lifestyle habits, physical characteristics and daily routines.</p>
+          <div className={style.how_components_container}>
+            <div className={style.how_component}>
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
