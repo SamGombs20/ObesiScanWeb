c16583ba98009dc27f35b6f8f7e0c642144ad26e
SamGombs20 <gombajosh18@gmail.com>
2026-01-06 15:04:07 +0300

    Update:About page development

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,25 @@
 import style from "../styles/about.module.css"
-const About =()=>{
-    return(
+const About = () => {
+    return (
         <div className={style.about_container}>
             <div className={style.title}>
                 <p>About Obesi<span>Scan</span></p>
             </div>
-            <div className={style.content}>
-                <p className={style.content_text}><strong><span>Obesi</span>Scan</strong> is a platform focused on predicting obesity levels while also featuring comprehensive obesity blogs. We are committed to delivering precise, current insights into obesity, including its causes, associated health risks, and effective prevention methods. Our goal is to increase public understanding of obesity's multifaceted nature and encourage healthier living through educational articles and valuable resources. We firmly believe that informed education on obesity empowers people to take control of their health and make better lifestyle choices.</p>
-                
+            <div className={style.content_container}>
+                <div className={style.content}>
+                    <p className={style.content_text}><strong><span>Obesi</span>Scan</strong> is a platform focused on predicting obesity levels while also featuring comprehensive obesity blogs. We are committed to delivering precise, current insights into obesity, including its causes, associated health risks, and effective prevention methods. Our goal is to increase public understanding of obesity's multifaceted nature and encourage healthier living through educational articles and valuable resources. We firmly believe that informed education on obesity empowers people to take control of their health and make better lifestyle choices.</p>
+                </div>
+                <div className={style.components_container}>
+                    <p>The Main Components are:</p>
+                    <div className={style.component}>
+                        <div className={style.component_img} ></div>
+                        <div className={style.component_content}>
+                            <p className={style.component_title}>Obesity Prediction Tool</p>
+                            <p>Our advanced machine learning model predicts obesity levels based on user input data. This data involves various factors such as age, gender, height, weight, and lifestyle habits. This helps users understand their risk factor and make informed decisions</p>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
