ff13061cb0ec7d99bbf84499699f508a70d0c317
SamGombs20 <gombajosh18@gmail.com>
2026-01-06 14:47:56 +0300

    Update:About page development

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,13 @@ import style from "../styles/about.module.css"
 const About =()=>{
     return(
         <div className={style.about_container}>
-
+            <div className={style.title}>
+                <p>About Obesi<span>Scan</span></p>
+            </div>
+            <div className={style.content}>
+                <p className={style.content_text}><strong><span>Obesi</span>Scan</strong> is a platform focused on predicting obesity levels while also featuring comprehensive obesity blogs. We are committed to delivering precise, current insights into obesity, including its causes, associated health risks, and effective prevention methods. Our goal is to increase public understanding of obesity's multifaceted nature and encourage healthier living through educational articles and valuable resources. We firmly believe that informed education on obesity empowers people to take control of their health and make better lifestyle choices.</p>
+                
+            </div>
         </div>
     )
 }
